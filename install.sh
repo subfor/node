5beb61c61aeb885b4b272807d67eb6dc2eb6e97f
alex <alex.kudelko@gmail.com>
2022-10-17 20:48:45 +0300

    first commit

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
-# trap ping 2
-
-ping 8.8.8.8
-echo "gfd"
-sleep 5
+wget -O minima_setup.sh https://raw.githubusercontent.com/minima-global/Minima/master/scripts/minima_setup.sh
+chmod +x minima_setup.sh && sudo ./minima_setup.sh -r 9232 -p 9231
+read -p "Enter ID: " id
+echo "Enter $id"
