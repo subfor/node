b648c3b4c3aada308cdd30ec19af767ebb7827f8
alex <alex.kudelko@gmail.com>
2022-10-24 15:17:50 +0300

    sui add

--- sui_update.sh
+++ sui_update.sh
@@ -30,3 +30,4 @@ if [[ `service suid status | grep active` =~ "running" ]]; then
 else
   echo -e "\033[0;31mYour Sui Node was not installed correctly, please reinstall.\033[0m"
 fi
+#. <(wget -qO- https://raw.githubusercontent.com/subfor/node/main/sui_update.sh)
